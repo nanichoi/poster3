701a96043badc5b0fa0f7b3abe7b3cc7cf755eb4
nanichoi <forevernani@naver.com>
2014-11-18 09:58:11 +0900

    3

--- script.js
+++ script.js
@@ -4,12 +4,13 @@
         var h = d.getHours();       
         if (h<12){            
             document.body.style.background = "url('image/back2.jpg') no-repeat center top";
-            document.getElementsByClassName("event")[0].innerHTML="이벤트마감"; 
-            document.getElementsByClassName("event")[0].style.color="gray";              
+            
+            document.getElementsByClassName("event")[0].innerHTML="이벤트중";
+            document.getElementsByClassName("event")[0].style.color="yellow";              
         }
         else{
             document.body.style.background = "url('image/back3.jpg') no-repeat center top";            
-            document.getElementsByClassName("event")[0].innerHTML="이벤트중";
-            document.getElementsByClassName("event")[0].style.color="yellow";             
+            document.getElementsByClassName("event")[0].innerHTML="이벤트마감"; 
+            document.getElementsByClassName("event")[0].style.color="gray";             
         }
     }
